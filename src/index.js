e3ed685be58c7e8b18590287f9660d077625ef79
Gianluca <gianlucadl92@gmail.com>
2024-04-12 10:33:33 +0200

    tutorial redux toolkit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import App from './App'
+import counterReducer from './features/counterSlice';
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+
